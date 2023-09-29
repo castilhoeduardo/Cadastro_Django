a3b409575405c242bd2bf385c615f0d36bf3dfb1
Eduardo Castilho Barbosa <eduardo.1220044@gmail.com>
2023-09-29 13:52:53 -0300

    .

---
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    #rota -> view resposável -> nome de referência
+    #cadastro.com
+    path('',views.home,name="home"),
+    #cadastro.com/usuarios
+    path('usuarios/',views.usuarios,name="listagem_usuarios"),
+     
+]
